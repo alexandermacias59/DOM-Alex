3a2e2a429bfb6e0b0779b923ae1faa28ee95101d
alexandermacias59 <alexandermacias59@gmail.com>
2025-11-25 13:24:54 +0100

    lezione del 25/11

--- script.js
+++ script.js
@@ -47,3 +47,72 @@ console.log(divOne.children);
 
 ////// SCRIVERE IL CONTENUTO
 
+//appendChild
+const newP = document.createElement("p");
+divOne.appendChild(newP);
+
+// createTextNode
+
+const node = document.createTextNode("quarto");
+newP.appendChild(node);
+
+//innerHTML
+
+newP.innerHTML = "quarto paragrafo";
+newP.innerHTML = "<strong>quarto paragrafo</strong>";
+
+//esempio
+
+const studenti1 = {name: "alexander", surname:"macias", age : 26, gender: "male"};
+const studenti2 = {name: "evelyn", surname:"medina", age : undefined, gender: "female"};
+
+//appendchild
+
+const emptyDiv = document.getElementById("empty-div");
+
+const cardDiv = document.createElement("div");
+cardDiv.id = "first-card";
+emptyDiv.appendChild(cardDiv);
+
+const nameP = document.createElement("p");
+const nameNode = document.createTextNode("nome: "+ studenti1.name);
+nameP.appendChild(nameNode);
+cardDiv.appendChild(nameP);
+
+const surnameP = document.createElement("p");
+const surnameNode = document.createTextNode("cognome: "+ studenti1.surname);
+surnameP.appendChild(surnameNode);
+cardDiv.appendChild(surnameP);
+
+const ageP = document.createElement("p");
+const ageNode = document.createTextNode("age: "+ studenti1.age);
+ageP.appendChild(ageNode);
+cardDiv.appendChild(ageP);
+
+const genderP = document.createElement("p");
+const genderNode = document.createTextNode("age: "+ studenti1.gender);
+genderP.appendChild(genderNode);
+cardDiv.appendChild(genderP);
+
+
+//innerHTML
+
+const htmlString = `<div class= "card">
+                        <p>nome: ${studenti2.name}</p>
+                        <p>cognome: ${studenti2.surname}</p>
+                        <p>eta: ${studenti2.age} </p>
+                        <p>genere: ${studenti2.gender}</p>
+                    </div>`
+
+console.log(htmlString);
+console.log(emptyDiv.innerHTML);
+// emptyDiv.innerHTML = emptyDiv.innerHTML + htmlString;
+emptyDiv.innerHTML += htmlString;
+
+
+//AGGIUNGERE CLASSI
+const card = document.getElementById("first-card");
+// card.className = "card";
+card.classList.add("card");
+card.classList.add("green-background");
+// card.classList.remove("green-background");
